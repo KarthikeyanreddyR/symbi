a01069e7fe974558e326f62f9094d44d50e0a54f
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-05-01 17:10:34 -0700

    updated models

--- angular-src/src/app/shared/models/caregiver/caregiver.ts
+++ angular-src/src/app/shared/models/caregiver/caregiver.ts
@@ -1,12 +1,13 @@
 import { Experience } from './experience';
+import { User } from '../user';
 
-export class Caregiver {
+export class Caregiver extends User {
   birthDate:Date;
   rate:number;
   certificates:File[];
   experience:Experience[];
   extraNotes:string;
   constructor() {
-
+    super();
   }
 }
--- angular-src/src/app/shared/models/parent/parent.ts
+++ angular-src/src/app/shared/models/parent/parent.ts
@@ -1,4 +1,4 @@
-import { User } from './user';
+import { User } from '../user';
 import { Family } from './family';
 
 export class Parent extends User {
