9d98e8ac3c67a68af3755a9a922fa03066a82970
Valerio Marcelli <marcelli@seattleu.edu>
2019-05-03 12:48:14 -0700

    updated pet schema

--- src/models/pet.ts
+++ src/models/pet.ts
@@ -1,5 +1,16 @@
 import { Document, Schema, Model, model} from "mongoose";
 
 const PetSchema: Schema = new Schema ({
-    
+    name: {
+        type: String
+    },
+    species: {
+        type: String
+    },
+    diet: {
+        type: [String]
+    },
+    extraNotes: {
+        type: [String]
+    }
 })
