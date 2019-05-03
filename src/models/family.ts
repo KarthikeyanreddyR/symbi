5ab00f02a092c813626d5989deaabd472347e0df
Valerio Marcelli <marcelli@seattleu.edu>
2019-05-03 12:41:06 -0700

    family schema changes

--- src/models/family.ts
+++ src/models/family.ts
@@ -2,9 +2,9 @@ import { Document, Schema, Model, model} from "mongoose";
 
 const FamilySchema: Schema = new Schema({
     children: {
-        type: [Children]
+        type: Schema.Types.ObjectId, ref: ['Children']
     },
     pets: {
-        type: [Pets]
+        type: Schema.Types.ObjectId, ref: ['Pets']
     }
 });
