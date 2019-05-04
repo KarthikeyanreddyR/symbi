3af6b82e97f8830030d1446ac3a868ac703248aa
Vic Chimenti <chimenti@seattleu.edu>
2019-05-03 19:32:17 -0700

    update views

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import { Request, Response } from "express";
-import { UserModel } from '../models/user';
-import path from 'path';
-
+import path from "path";
+import { UserModel } from "../models/user";
 
 class Routes {
     public router: express.Router;
@@ -14,53 +13,54 @@ class Routes {
 
     private init(): void {
         this.router.get("/", (req: Request, res: Response) => {
-            res.sendFile(path.join(__dirname + '../../views/index.html'));
+            res.sendFile(path.join(__dirname + "../../views/index.html"));
         });
 
-        this.router.get("/register", (req: Request, res:Response) => {
-            res.sendFile(path.join(__dirname + '../../views/register.html'));
+        this.router.get("/register", (req: Request, res: Response) => {
+            res.sendFile(path.join(__dirname + "../../views/register.html"));
         });
 
-        this.router.post('/register', (req: Request, res: Response) => {
-            let newUser = new UserModel();
+        this.router.post("/register", (req: Request, res: Response) => {
+            const newUser = new UserModel();
             newUser.firstName = req.body.name;
             newUser.email = req.body.email;
             newUser.password = req.body.password;
 
             try {
                 newUser.save((err, user) => {
-                    if(err) throw err;
-                    console.log('registered user');
+                    if (err) { throw err; }
+                    console.log("registered user");
                     res.json({
                         success: true,
-                        msg: 'Registered user',
-                        user : user
+                        // tslint:disable-next-line:object-literal-sort-keys
+                        msg: "Registered user",
+                        user,
                     });
-                })
-                
+                });
+
             } catch (error) {
                 res.json({
                     success: false,
-                    msg: 'Failed to register'
+                    // tslint:disable-next-line:object-literal-sort-keys
+                    msg: "Failed to register",
                 });
             }
         });
 
-        this.router.get('/login', ((req: Request, res: Response) => {
-            res.sendFile(path.join(__dirname + '../../views/login.html'));
+        this.router.get("/login", ((req: Request, res: Response) => {
+            res.sendFile(path.join(__dirname + "../../views/login.html"));
         }));
 
         // Authenticate
-        this.router.post('/auth', (req: Request, res: Response) => {
-            res.send('Auth');
+        this.router.post("/auth", (req: Request, res: Response) => {
+            res.send("Auth");
         });
 
         // Profile
-        this.router.get('/profile', (req: Request, res: Response) => {
-            res.send('profile');
+        this.router.get("/profile", (req: Request, res: Response) => {
+            res.send("profile");
         });
     }
 }
-
 
 export default new Routes().router;
