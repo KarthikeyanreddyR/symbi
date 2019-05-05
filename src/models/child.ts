1cab0db8af6ad317dab8bc319e52bb6f2778374d
Valerio Marcelli <marcelli@seattleu.edu>
2019-05-05 12:18:03 -0700

    documention for child.ts

--- src/models/child.ts
+++ src/models/child.ts
@@ -1,3 +1,15 @@
+/**
+ * Filename: child.ts
+ * Created: 5.3.2019
+ * Description: Mongoose Schema for the Child model. Child holds the 
+ *              information for children that belong to a Parent's
+ *              Family. Other than basic bio information, a Child
+ *              has a special needs section so that a Parent may
+ *              highlight needs that a caretaker needs to take note
+ *              of when taking care of a Child. Children also have a
+ *              sleepTime, for when they are supposed to go to bed, and
+ *              a diet section for anything that they should be eating.
+ */
 import { Document, Schema, Model, model} from "mongoose";
 
 export const ChildSchema: Schema = new Schema ({
