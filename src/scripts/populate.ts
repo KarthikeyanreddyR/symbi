aa054f78b2f30db9cbee6647d6c68cc801c93e5d
Valerio Marcelli <marcelli@seattleu.edu>
2019-05-06 15:45:21 -0700

    more populate script

--- src/scripts/populate.ts
+++ src/scripts/populate.ts
@@ -10,5 +10,22 @@ let John = new UserModel({
     profileImage: undefined,
     email: "notacoward@outlaw.com",
     password: "edgarrosssucks1",
-    address: ""
+    address: "1776 Texas Rd",
+    phoneNumber: undefined,
+    bio: "Outlaw bounty hunter seeking caregiver for baby boy",
+    profiles: undefined
+});
+
+//Mary is a caregiver
+let Mary = new UserModel({
+    firstName: "John",
+    lastName: "Marston",
+    gender: "Male",
+    profileImage: undefined,
+    email: "notacoward@outlaw.com",
+    password: "edgarrosssucks1",
+    address: "1776 Texas Rd",
+    phoneNumber: undefined,
+    bio: "Outlaw bounty hunter seeking caregiver for baby boy",
+    profiles: undefined
 });
