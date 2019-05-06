156ce6ba8dc3e81de6be8e27432c65459b581f65
Valerio Marcelli <marcelli@seattleu.edu>
2019-05-06 14:35:29 -0700

    IJob documentation

--- src/interfaces/IJob.ts
+++ src/interfaces/IJob.ts
@@ -1,3 +1,11 @@
+/**
+ * Filename: IJob.ts
+ * Created: 5.6.2019
+ * Description: Interface for the Job schema.
+ *              This interface mirrors the job.ts Schema
+ *              All fields in a Job are required for a Job to
+ *              exist, a Job is then collected under JobListing
+ */
 import { Document } from "mongoose";
 import { JobType, JobStatus } from "./enums";
 import { IEvent } from "./IEvent";
