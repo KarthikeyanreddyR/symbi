9eb5d2323addf30015e9756de5cc963a2e0ab924
Valerio Marcelli <marcelli@seattleu.edu>
2019-05-08 16:09:20 -0700

    IReview documentation

--- src/interfaces/IReview.ts
+++ src/interfaces/IReview.ts
@@ -1,5 +1,24 @@
-import { Document } from "mongoose";
+/**
+ * Filename: IReview.ts
+ * Created: 5.7.2019
+ * Description: An interface for the Review class. Reviews
+ * function as a way for parents to rate caregivers that they
+ * have hired.
+ */
 
+ /**
+  * Interface for the Review class.
+  * reviwerID - The identification number of the person posting
+  *             the review.
+  * revieweeID - The identification number of the person the review
+  *              is about.
+  * reviewTitle - Title of the review.
+  * reviewDate - Date the review was posted
+  * starRating - The rating the reviewer is giving the reviewee
+  *              minimum = 1, maximum = 5
+  * reviewNotes - Optional additional notes that a reviewer can add to a 
+  *               review.
+  */
 export interface IReview {
     reviewerID: string;
     revieweeID: string;
