ce8480c2475aa7c550e8a6967c10e987827cd33b
Victor Chimenti <chimenti@seattleu.edu>
2019-05-23 17:37:22 -0700

    get one user test fully functional - passes

--- src/tests/test_routes.js
+++ src/tests/test_routes.js
@@ -11,19 +11,31 @@ const should = chai.should();
 describe("Test GET /api/users/:userId", function () {
 
     let _response;
+    let _httpResponse;
     before(function (done) {
         chai.request('http://localhost:3000')
-            .get('/api/users/_id')
+            .get('/api/users/5cd386aa1a05652e9c7eeaf3')
             .end(function (err, res) {
                 expect(err).to.be.null;
-                _response = res;
+                _httpResponse = res;
+                _response = res.body;
                 done();
             });
     });
 
+    /***   Check for correct status code and json object   ***/
     it('status must be 200', function () {
-        expect(_response).to.have.status(200);
+        expect(_httpResponse).to.have.status(200);
+        expect(_httpResponse).to.be.json;
     });
+
+    /***   Check for required properties in the user json object   ***/
+    it('validate email, password properties', function () {
+        expect(_response).to.have.property('success');
+        expect(_response).to.have.property('data');
+        expect(_response.data).to.have.property('email').that.is.a('String');
+        expect(_response.data).to.have.property('password').that.is.a('String');
+    })
 });
 
 /***   Mocha Test 2 : Get List Objects   ***/
