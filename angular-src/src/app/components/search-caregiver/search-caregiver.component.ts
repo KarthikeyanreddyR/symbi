157937b56187d4980d467af5d8f970a3f8a1cf8e
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-06-06 15:27:19 -0700

    component, routing for search caregiver

--- angular-src/src/app/components/search-caregiver/search-caregiver.component.ts
+++ angular-src/src/app/components/search-caregiver/search-caregiver.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-search-caregiver',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchCaregiverComponent implements OnInit {
 
-  constructor() { }
+  constructor( private userService: UserService) { }
+
+  user$: any;
+  reviews$: any;
+
+  fetchError: boolean;
 
   ngOnInit() {
+    this.LoadData();
+  }
+
+  public LoadData(): void {
+    this.fetchError = false;
+    this.userService.GetAllCaregiversWithReviewData().subscribe(res => {
+      console.log(res);
+      if(res.success) {
+        this.user$ = res.data['users'];
+        this.reviews$ = res.data['reviews'];
+      } else {
+        this.user$ = [];
+        this.reviews$ = [];
+      }
+    },
+    err => {
+      this.fetchError = true;
+    });
   }
 
 }
