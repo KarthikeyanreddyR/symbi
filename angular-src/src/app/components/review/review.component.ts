5642f29b98ed5a6864ec7b27eaa2226d09fe3008
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-06-07 16:01:11 -0700

    styled review component

--- angular-src/src/app/components/review/review.component.ts
+++ angular-src/src/app/components/review/review.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, Validators, FormBuilder, FormGroup} from '@angular/forms';
-import { UserService } from 'src/app/services/user.service';
-
+import { Component, OnInit, Input } from "@angular/core";
+import {
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormGroup
+} from "@angular/forms";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-review',
-  templateUrl: './review.component.html',
-  styleUrls: ['./review.component.css']
+  selector: "app-review",
+  templateUrl: "./review.component.html",
+  styleUrls: ["./review.component.css"]
 })
 export class ReviewComponent implements OnInit {
-  
- 
+  reviews$: any = [];
 
-  Reviews = []
-  
-  constructor(private formBuilder: FormBuilder,private userservice: UserService) { }
-  
+  constructor(private userservice: UserService) {}
+
   ngOnInit() {
-  this.userservice.GetAllReviewsByUser("5ce3581d5fba742e68b35972").subscribe(res => {
-    console.log(res);
-    this.Reviews = res.data;
-   
-  },
-  err => {
-    // error handling
-  },
-  () => {
-
-  })
-}
-    
+    this.userservice.GetAllReviewsByUser("5ce3581d5fba742e68b35972").subscribe(
+      res => {
+        console.log(res);
+        this.reviews$ = res.data;
+      },
+      err => {
+        // error handling
+        console.log(err);
+      },
+      () => {}
+    );
   }
 
-
+  public getStartRatingsArray(rating: number) {
+    let finArr = [false, false, false, false, false];
+    for (let i = 0; i < rating; i++) {
+      finArr[i] = true;
+    }
+    return finArr;
+  }
+}
