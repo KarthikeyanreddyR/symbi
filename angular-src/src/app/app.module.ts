e362df927c52fe0e910993182131f327f2ee402e
Victor Chimenti <chimenti@seattleu.edu>
2019-06-07 12:21:55 -0700

    caregiver calendar clickable

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 // components
 import { AppComponent } from './app.component';
@@ -56,11 +58,15 @@ import { AppPasswordDirective } from './shared/directives/app-password.directive
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
+    NgbModalModule,
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    NgbModalModule.forRoot(),
+    FlatpickrModule.forRoot(),
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
