25daf87e45f8efe1b48536022b281f7e788502a1
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-06-06 23:26:59 -0700

    caregivers fetch/display, reviews for caregivers

--- angular-src/src/app/components/search-caregiver/search-caregiver.component.ts
+++ angular-src/src/app/components/search-caregiver/search-caregiver.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { ScheduleCaregiverService } from 'src/app/services/schedule-caregiver.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-caregiver',
@@ -8,12 +10,16 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SearchCaregiverComponent implements OnInit {
 
-  constructor( private userService: UserService) { }
+  constructor(private userService: UserService,
+    private scheduleCaregiverService: ScheduleCaregiverService,
+    private router: Router) { }
 
   user$: any;
   reviews$: any;
 
   fetchError: boolean;
+
+  userReviews$: [any];
 
   ngOnInit() {
     this.LoadData();
@@ -23,7 +29,7 @@ export class SearchCaregiverComponent implements OnInit {
     this.fetchError = false;
     this.userService.GetAllCaregiversWithReviewData().subscribe(res => {
       console.log(res);
-      if(res.success) {
+      if (res.success) {
         this.user$ = res.data['users'];
         this.reviews$ = res.data['reviews'];
       } else {
@@ -31,9 +37,41 @@ export class SearchCaregiverComponent implements OnInit {
         this.reviews$ = [];
       }
     },
-    err => {
-      this.fetchError = true;
+      err => {
+        this.fetchError = true;
+      });
+  }
+
+  public getStarRatings(id: string) {
+    let _arr: [] = this.getReviewsForId(id).map(rev => {
+      return rev['starRating'];
     });
+    let totalRating = _arr.reduce((a, b) => a + b, 0);
+    let avgRating = Math.ceil(totalRating / 5);
+    return this.getStartRatingsArray(avgRating);
+  }
+
+  public getStartRatingsArray(rating: number) {
+    let finArr = [false, false, false, false, false];
+    for (let i = 0; i < rating; i++) {
+      finArr[i] = true;
+    }
+    return finArr;
+  }
+
+  public getReviewsForId(id: string) {
+    return this.reviews$.filter(rev => {
+      return rev['revieweeID'] == id;
+    });
+  }
+
+  public showDetailedReviews(id: string) {
+    this.userReviews$ = this.getReviewsForId(id);
+  }
+
+  public scheduleCaregiver(user: any) {
+    this.scheduleCaregiverService.setCaregiver(user);
+    this.router.navigate(['/caregivers/schedule']);
   }
 
 }
