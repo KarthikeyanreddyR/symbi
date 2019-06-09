74ea81988b87f78c15eeb150528c46680d98e161
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-06-09 01:08:33 -0700

    auth guard service

---
+++ angular-src/src/app/services/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Router, CanActivate } from "@angular/router";
+import { CommonUtilsService } from './common-utils.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private commonUtilsService: CommonUtilsService, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
+    return this.commonUtilsService.signedInUser$.pipe(map(res => {
+      return true;
+    }))
+  }
+}
