989dcbc77de2f04955e98fbde601d069e238b4ce
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-06-10 16:01:15 -0700

    post open job

--- angular-src/src/app/components/job/post-open-job/post-open-job.component.ts
+++ angular-src/src/app/components/job/post-open-job/post-open-job.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { JobService } from 'src/app/services/job.service';
+import { CommonUtilsService } from 'src/app/services/common-utils.service';
+import { Subscription } from 'rxjs';
 
+// Initialize JQuery
 declare var $: any;
 
 @Component({
@@ -9,21 +14,94 @@ declare var $: any;
 })
 export class PostOpenJobComponent implements OnInit {
 
-  constructor() { }
+  private subscription: Subscription = new Subscription();
+
+  private loggedInUser: any;
+  postSuccess: boolean;
+
+  constructor(private formBuilder: FormBuilder, private jobService: JobService, private commonUtilsService: CommonUtilsService) {
+    this.subscription.add(this.commonUtilsService.signedInUser$.subscribe(res => {
+      this.loggedInUser = res
+    }, err => {
+      // error handling
+    }));
+   }
+
+  postOpenJobForm: FormGroup = this.formBuilder.group({
+    jobName: ['', [Validators.required]],
+    jobStartTime: ['', [Validators.required]],
+    jobEndTime: ['', [Validators.required]],
+    jobNotes: ['']
+  });
+
+  get jobName() {
+    return this.postOpenJobForm.get('jobName');
+  }
+  get jobStartTime() {
+    return this.postOpenJobForm.get('jobStartTime');
+  }
+  get jobEndTime() {
+    return this.postOpenJobForm.get('jobEndTime');
+  }
+  get jobNotes() {
+    return this.postOpenJobForm.get('jobNotes');
+  }
 
   ngOnInit() {
+    let self = this;
     $('#startDate').datetimepicker({
       sideBySide: true,
-      locale: 'en'
+      locale: 'en',
+      format: 'MM/DD/YYYY hh:mm A Z'
     });
     $('#endDate').datetimepicker({
       sideBySide: true,
-      locale: 'en'
+      locale: 'en',
+      format: 'MM/DD/YYYY hh:mm A Z'
+    });
+    $('#startDate').on("change.datetimepicker", function (e) {
+      if(e.date) {
+        let _d = e.date.format('MM/DD/YYYY hh:mm A Z')
+        self.postOpenJobForm.patchValue({
+          jobStartTime: _d
+        });
+      }
+    });
+    $('#endDate').on("change.datetimepicker", function (e) {
+      if(e.date) {
+        let _d = e.date.format('MM/DD/YYYY hh:mm A Z')
+        self.postOpenJobForm.patchValue({
+          jobEndTime: _d
+        });
+      }
     });
   }
 
-  public postOpenJob() {
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
+  public postOpenJob() {
+    this.postSuccess = false;
+    console.log(this.postOpenJobForm.value);
+
+    let job: any = {
+      userId: this.loggedInUser['_id'],
+      createdAt: new Date().toDateString()
+    }
+
+    job = Object.assign({}, job, this.postOpenJobForm.value)
+
+    console.log(job);
+
+    this.jobService.postOpenJob(job).then(res => {
+      console.log(res);
+      if(res.success) {
+        this.postSuccess = true;
+      }
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
--- angular-src/src/app/services/job.service.ts
+++ angular-src/src/app/services/job.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../shared/models/user';
+import { ApiResponse } from '../shared/interfaces/response';
+import { CommonUtilsService } from './common-utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllJobsForUser(id:string): Observable<Object> {
+    return this.httpClient.get<Object>(`${CommonUtilsService.getAbsoluteUrl()}/jobs/jobsByUser/${id}`);
+  }
+
+  public postOpenJob(job: any): Promise<ApiResponse> {
+    return this.httpClient.post<ApiResponse>(`${CommonUtilsService.getAbsoluteUrl()}/jobs/openJob`, job).toPromise();
+  }
+
 }
