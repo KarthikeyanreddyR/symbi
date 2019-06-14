5260d392879461631f222bae15ff6146dbb1a1d6
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-06-13 20:47:55 -0700

    code clean up

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,7 +1,7 @@
 import localConfig from '../env'
 
 export = {
-    database: process.env.CUSTOMCONNSTR_MONGO_CONN_STR || 'mongodb://rokkamkarthi:admin@cluster0-shard-00-00-plmv5.azure.mongodb.net:27017,cluster0-shard-00-01-plmv5.azure.mongodb.net:27017,cluster0-shard-00-02-plmv5.azure.mongodb.net:27017/symbi?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    database: process.env.CUSTOMCONNSTR_MONGO_CONN_STR || localConfig.database.host,
     googleOAuth: {
         clientID: process.env.APPSETTING_GOOGLE_CLIENT_ID || localConfig.google.clientID,
         clientSecret: process.env.APPSETTING_GOOGLE_SECRET || localConfig.google.clientSecret
--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,8 @@ export = {
     google: {
         clientID: 'ur-client-id',
         clientSecret: 'ur-client-secret'
+    },
+    database: {
+        host: 'ur-data-nase url'
     }
 }
